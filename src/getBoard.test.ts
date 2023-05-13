034411536e910e50f1d3219fbc11136014baf57a
Silvia <silvia.garbas@gmail.com>
2023-05-13 20:27:47 +0200

    Added content

--- src/getBoard.test.ts
+++ src/getBoard.test.ts
@@ -2,7 +2,7 @@ import {getBoard} from "./getBoard";
 
 describe("Given a getBoard function", () => {
   describe("When it receives 3 and 3", () => {
-    test("Then it should return a board length of 5", () => {
+    test("Then it should return a board length of 3", () => {
       const rows = 3;
       const cols = 3;
       const expectedBoardLength = 3;
